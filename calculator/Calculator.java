1761eee5c0f8074354331fe3f9fcd60fdd84f2cc
gianca1994 <gianca9405@gmail.com>
2021-04-14 20:31:04 -0500

    Refactor

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -3,6 +3,6 @@ package calculator;
 public class Calculator {
 
     public static void main(String[] args) {
-
+        
     }
 }
--- calculator/Printer.java
+++ calculator/Printer.java
@@ -20,12 +20,8 @@ public class Printer {
      */
     public static void print(Object... args) {
 
-        if (args.length > 1) {
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-            }
-        } else {
-            System.out.println(args[0]);
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i]);
         }
 
     }
