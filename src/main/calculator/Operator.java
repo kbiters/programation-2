827e2dc2214149d225109ce57625f512d03d7269
Juan Pablo <juampigarcia2014@gmail.com>
2021-04-18 06:19:45 -0300

    feat: :zap: Create OperatorMap Enum

--- src/main/calculator/Operator.java
+++ src/main/calculator/Operator.java
@@ -1,8 +1,33 @@
 package calculator;
 
+import java.util.HashMap;
+
 public enum Operator {
-    SUM("+")
-    SUBTRACT("+")
-    MULTIPLY("*")
-    DIVIDE("/")
+    SUM("+"),
+    SUBTRACT("+"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+
+    private final String value;
+
+    private static final Map<String, Operator> operatorMap = new HashMap<String, Operator>();
+
+    static {
+        for (Operator op : Operator.values()) {
+            operatorMap.put(op.getValue(), op);
+        }
+    }
+
+    private Operator(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static Operator get(String value) {
+        return operatorMap.get(value);
+    }
+
 }
