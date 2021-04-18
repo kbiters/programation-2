7ed748daee8a0f320196914f6358c80e6279400e
bthecs <fl.gimenez@alumno.um.edu.ar>
2021-04-18 07:46:56 -0300

    Working calculator

--- src/main/calculator/calculator/Menu.java
+++ src/main/calculator/calculator/Menu.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Scanner;
+
 /**
  * Class defined to execute the program instructions.
  *
@@ -8,11 +10,25 @@ package calculator;
  **/
 public class Menu {
 
+    public void run(){
+        Scanner reader = new Scanner(System.in);
+        this.showInputNumber();
+        double firstNumber = reader.nextDouble();
+        this.showInputNumber();
+        double secondNumber = reader.nextDouble();
+        this.showInputOperator();
+        String operator = reader.next();
+        Calculator calc = new Calculator();
+        double result = calc.math(firstNumber, secondNumber, operator);
+        this.showResult(firstNumber, secondNumber, operator, result);
+    }
+
     /**
      * Function to print on screen the constant "INPUT_NUMBER".
      */
     public void showInputNumber() {
         Printer.print(Constant.INPUT_NUMBER);
+
     }
 
     /**
---
+++ src/main/calculator/calculator/Operator.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Operator {
+    SUM("+"), SUBTRACT("-"), MULTIPLY("*"), DIVIDE("/");
+
+    private static Map<String, Operator> operatorMap = new HashMap<String, Operator>();
+
+    private final String value;
+
+    static {
+        for (Operator op : Operator.values()) {
+            operatorMap.put(op.value, op);
+        }
+    }
+
+    private Operator(String value) {
+        this.value = value;
+    }
+
+    public static Operator getOperator(String key) {
+        return operatorMap.get(key);
+    }
+
+}
